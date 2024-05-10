7e855ec095188c87e193dbb2fb8d196c6bea7c84
Leon Altamirano <167282041+LeonAD19@users.noreply.github.com>
2024-05-10 02:30:09 -0500

    sudoku.h

---
+++ include.txt/sudoku.h
@@ -0,0 +1,8 @@
+
+#ifndef SUDOKUPROJECT_SUDOKU_H
+#define SUDOKUPROJECT_SUDOKU_H
+void printBoard(int**, int, int, int);
+bool isValid(int**, int, int, int);
+bool solveBoard(int**, int, int);
+int** generateBoard();
+#endif //SUDOKUPROJECT_SUDOKU_H
